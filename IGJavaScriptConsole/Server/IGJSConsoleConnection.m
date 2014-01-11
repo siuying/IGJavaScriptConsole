816e18974b14dae0b83a8dad0c4b2a513abf367a
Francis Chong <francis@ignition.hk>
2014-01-11 17:49:32 +0800

    do not use revision in linner.

--- IGJavaScriptConsole/Server/IGJSConsoleConnection.m
+++ IGJavaScriptConsole/Server/IGJSConsoleConnection.m
@@ -5,6 +5,10 @@
 //  Created by Francis Chong on 1/11/14.
 //  Copyright (c) 2014 Francis Chong. All rights reserved.
 //
+#import "GCDAsyncSocket.h"
+#import "HTTPMessage.h"
+#import "HTTPResponse.h"
+#import "HTTPDynamicFileResponse.h"
 
 #import "DDLog.h"
 #import "IGJSConsoleConnection.h"
@@ -19,6 +23,33 @@ static const int jsConsoleLogLevel = LOG_LEVEL_VERBOSE;
 
 -(NSObject<HTTPResponse>*) httpResponseForMethod:(NSString *)method URI:(NSString *)path {
     DDLogDebug(@"%@ %@", method, path);
+    
+    if ([path isEqualToString:@"/WebSocketTest2.js"]) {
+        // The socket.js file contains a URL template that needs to be completed:
+        //
+        // ws = new WebSocket("%%WEBSOCKET_URL%%");
+        //
+        // We need to replace "%%WEBSOCKET_URL%%" with whatever URL the server is running on.
+        // We can accomplish this easily with the HTTPDynamicFileResponse class,
+        // which takes a dictionary of replacement key-value pairs,
+        // and performs replacements on the fly as it uploads the file.
+        
+        NSString *wsLocation;
+        NSString *scheme = [asyncSocket isSecure] ? @"wss" : @"ws";
+        NSString *wsHost = [request headerField:@"Host"];
+        if (wsHost == nil) {
+            NSString *port = [NSString stringWithFormat:@"%hu", [asyncSocket localPort]];
+            wsLocation = [NSString stringWithFormat:@"%@://localhost:%@/service", scheme, port];
+        } else {
+            wsLocation = [NSString stringWithFormat:@"%@://%@/service", scheme, wsHost];
+        }
+        NSDictionary *replacementDict = [NSDictionary dictionaryWithObject:wsLocation forKey:@"WEBSOCKET_URL"];
+        return [[HTTPDynamicFileResponse alloc] initWithFilePath:[self filePathForURI:path]
+                                                   forConnection:self
+                                                       separator:@"%%"
+                                           replacementDictionary:replacementDict];
+    }
+
     return [super httpResponseForMethod:method URI:path];
 }
 
---
+++ Rakefile
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+desc "Build js app"
+task :'js:build' do
+  `cd JavaScriptApp && linner build`
+end
